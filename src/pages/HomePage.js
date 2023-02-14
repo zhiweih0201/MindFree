da3a8fb754033689423e29f6f952f82da98deca9
zhiweih0201 <zhiweih@seas.upenn.edu>
2023-02-14 13:12:52 -0500

    profile route

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,95 +11,101 @@ import image_setting from "../image/WechatIMG217.jpeg"
 
 export default function HomePage(props) {
 
-    return (
-        <div className="Home">
-        {/*<div style={{ backgroundImage:`url(${image})`, height:780, width:4000, left:1000, backgroundRepeat:"no-repeat", backgroundSize:"contain"}}> */}
-        <div className="right-background">
-          <div className="main-text">
-            <p>
-              MindFree
-            </p>
-          </div>
-          <span className="explain-text">Unbothered. Moisturized. In My Lane. Focused. Flourishing.</span>
+  const navigate = useNavigate();
+  
+  function loadProfile() {
+    return navigate('/profile')
+  }
+
+  return (
+    <div className="Home">
+      {/*<div style={{ backgroundImage:`url(${image})`, height:780, width:4000, left:1000, backgroundRepeat:"no-repeat", backgroundSize:"contain"}}> */}
+      <div className="right-background">
+        <div className="main-text">
+          <p>
+            MindFree
+          </p>
         </div>
-        <div className="left-bar">
-          <Button className="sign-in" style={{
-            background: '#F3E0B5',
-            color: '#FF996D',
-            fontFamily: 'Inter',
-            fontStyle: 'normal',
-            fontWeight: 600,
-            fontSize: 26,
-            textTransform: 'none',
-            padding: 0,
-            margin: 0
-          }}>
-            Sign In
-          </Button>
-          <Button className="feed" style={{
-            background: '#F3E0B5',
-            color: '#FF996D',
-            fontFamily: 'Inter',
-            fontStyle: 'normal',
-            fontWeight: 600,
-            fontSize: 26,
-            textTransform: 'none',
-            padding: 0,
-            margin: 0
-          }}>
-            Feed
-          </Button>
-          <Button className="chats" style={{
-            background: '#F3E0B5',
-            color: '#FF996D',
-            fontFamily: 'Inter',
-            fontStyle: 'normal',
-            fontWeight: 600,
-            fontSize: 26,
-            textTransform: 'none',
-            padding: 0,
-            margin: 0
-          }}>
-            Chats
-          </Button>
-          <Button className="forum" style={{
-            background: '#F3E0B5',
-            color: '#FF996D',
-            fontFamily: 'Inter',
-            fontStyle: 'normal',
-            fontWeight: 600,
-            fontSize: 26,
-            textTransform: 'none',
-            padding: 0,
-            margin: 0
-          }}>
-            Forums
-          </Button>
-          <Button className="therapist" style={{
-            background: '#F3E0B5',
-            color: '#FF996D',
-            fontFamily: 'Inter',
-            fontStyle: 'normal',
-            fontWeight: 600,
-            fontSize: 26,
-            textTransform: 'none',
-            padding: 0,
-            margin: 0
-          }}>
-            Therapists
-          </Button>
-          <Button> <img className="set-image" src={image_setting} alt="setting_image" width="53"
-            height="50" />
-          </Button>
-          <img
-            className="penn-image"
-            src={image_penn}
-            alt="penn"
-            width="126"
-            height="40"
-          />
-        </div>
+        <span className="explain-text">Unbothered. Moisturized. In My Lane. Focused. Flourishing.</span>
       </div>
-    );
+      <div className="left-bar">
+        <Button className="sign-in" style={{
+          background: '#F3E0B5',
+          color: '#FF996D',
+          fontFamily: 'Inter',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: 26,
+          textTransform: 'none',
+          padding: 0,
+          margin: 0
+        }}>
+          Sign In
+        </Button>
+        <Button className="feed" style={{
+          background: '#F3E0B5',
+          color: '#FF996D',
+          fontFamily: 'Inter',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: 26,
+          textTransform: 'none',
+          padding: 0,
+          margin: 0
+        }} onClick={loadProfile}>
+          Profile
+        </Button>
+        <Button className="chats" style={{
+          background: '#F3E0B5',
+          color: '#FF996D',
+          fontFamily: 'Inter',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: 26,
+          textTransform: 'none',
+          padding: 0,
+          margin: 0
+        }}>
+          Chats
+        </Button>
+        <Button className="forum" style={{
+          background: '#F3E0B5',
+          color: '#FF996D',
+          fontFamily: 'Inter',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: 26,
+          textTransform: 'none',
+          padding: 0,
+          margin: 0
+        }}>
+          Forums
+        </Button>
+        <Button className="therapist" style={{
+          background: '#F3E0B5',
+          color: '#FF996D',
+          fontFamily: 'Inter',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: 26,
+          textTransform: 'none',
+          padding: 0,
+          margin: 0
+        }}>
+          Therapists
+        </Button>
+        <Button> <img className="set-image" src={image_setting} alt="setting_image" width="53"
+          height="50" />
+        </Button>
+        <img
+          className="penn-image"
+          src={image_penn}
+          alt="penn"
+          width="126"
+          height="40"
+        />
+      </div>
+    </div>
+  );
 
 }
