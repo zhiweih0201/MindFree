85a80c727a30cfe2adc7594187e4a6b95609ac51
laursp <118462444+laursp@users.noreply.github.com>
2023-03-25 20:10:53 -0400

    setting styles

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -4,27 +4,99 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { Grid } from '@mantine/core';
 import '../styles/setting.scss';
+import imgBg from '../image/WechatIMG215.jpeg'
+import rebutton from '../assets/icons8-return-48.png';
 
 export default function Setting(props) {
 
-    return (
-        <Grid>
-            <Grid.Col span={2}>
-                <div className = 'background'>
-                    <div className='setting-background'>
-                        <div className='setting-title-container'>
-                            <p className='title'>Setting</p>
-                        </div>
-                        <SettingModal />
-                    
-                    </div>
-                </div>
-                
-            </Grid.Col>
-            <Grid.Col span={8}>
+    const navigate = useNavigate();
 
-            </Grid.Col>
-        </Grid>
+    function loadHomepage() {
+        return navigate('/homepage')
+    }
+
+
+    return (<div className="row setting">
+                <div className="left" 
+                style={{
+                    
+              
+                    backgroundImage: "url(" + { imgBg } + ")"
+                
+                }}
+                
+                
+                >
+                     <Button>
+                    <img className="" src={rebutton} alt="my image" onClick={loadHomepage} />
+                </Button>
+
+        
+                   
+                
+                </div>
+                <div className='right'>
+                    <h2>Security</h2>
+                    <div className="round-box">
+                    
+                        <fieldset>
+                            <label>question ?</label>
+                            <select>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <label>question ?</label>
+                            <select>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <label>question ?</label>
+                            <select>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                            </select>
+                        </fieldset>
+
+
+                    </div>
+
+
+
+
+                    <h2>Notification</h2>
+                    <div className="round-box">
+                    
+                        <fieldset>
+                            <label>question ?</label>
+                            <select>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <label>question ?</label>
+                            <select>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <label>question ?</label>
+                            <select>
+                                <option>option 1</option>
+                                <option>option 2</option>
+                            </select>
+                        </fieldset>
+
+
+                    </div>
+
+                </div>
+            </div>
     );
 
 }
